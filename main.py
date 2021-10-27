43589e015c84a90c74f1389ed523f62fe26ca482
schus <ms113429@student.sgh.waw.pl>
2021-10-27 16:10:57 +0200

    Add gitignore

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Umschalt+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    '''
+    what print_hi is supposed to do
+    :param name: # What this parameter means
+    :return: # what it should do
+    '''
+    # Use a breakpoint in the code line below to debug your script.
+    # print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+    # print('Hi, {}'.format(name))  # Press Strg+F8 to toggle the breakpoint.
+    print('Hi, '+name)  # Press Strg+F8 to toggle the breakpoint.
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
