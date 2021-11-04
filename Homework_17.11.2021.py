4ea661b443a8d984bf3be30128ea7a600a65104c
schus <ms113429@student.sgh.waw.pl>
2021-11-04 16:40:32 +0100

    Homework_second try

--- Homework_17.11.2021.py
+++ Homework_17.11.2021.py
@@ -1,5 +1,5 @@
-N=101
-for num in range(1,N):
+N1=101
+for num in range(1,N1):
     for i in range(2,num):
         if (num%i==0):
             break
@@ -7,4 +7,12 @@ for num in range(1,N):
             print(num)
             break
 
-#not bad but N has to be the number of the prime numbers displayed
+N = 10
+for i in range(2,N+1):
+    for j in range(2,i):
+        if(i%j == 0):
+            break
+    else:
+        print(i)
+
+#the second version is better but we didn't had this either
