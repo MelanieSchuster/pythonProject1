ce8a540ba3e4ac060005a2ab465702dd55dd886a
schus <ms113429@student.sgh.waw.pl>
2021-11-22 18:34:06 +0100

    Homework changes to the new exercise

--- Homework_10.11.2021.py
+++ Homework_10.11.2021.py
@@ -1,17 +1,22 @@
-#https://www.delftstack.com/howto/python/python-count-words-in-string/
+import re
+
 if __name__ == '__main__':
-    def word_count(str):
-        counts = dict()
-        words = str.split()
-
+    f = "text.txt"
+    #the file has to be in the same directonary as the project
+    text = open(f, 'r')
+    #open the file in read mode
+    d = dict()
+    for line in text:
+        #remove special symbols and put all words in lower case
+        line = line.strip()
+        line = re.sub(r"[^a-zA-Z ]", "", line)
+        line = line.lower()
+        words = line.split(" ")
         for word in words:
-            if word in counts:
-                counts[word] += 1
+            if word in d:
+                d[word] = d[word]+1
             else:
-                counts[word] = 1
-
-        return counts
-
-    print( word_count('the quick brown fox jumps over the lazy dog.'))
-
-#einfach nur aus dem Internet kopiert, bitte selbst versuchen und etwas ändern
+                d[word] = 1
+                #count one more if the word is in the dictionary, otherwise put the word into the dictionary
+    print(d)
+    #print the dictionary
