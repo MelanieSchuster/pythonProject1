3b876d043e4aceaf1271fb2293ce384bd6319ef5
schus <ms113429@student.sgh.waw.pl>
2021-12-01 16:53:39 +0100

    folder exercise

--- folders.py
+++ folders.py
@@ -7,8 +7,12 @@ print(python_folder)
 sum_bytes = 0
 for file in os.listdir(python_folder):
     if os.path.isfile(file):
+        #folders are not treated
         print(file)
         #print(os.stat(file))
         sum_bytes += os.path.getsize(file)
         print(os.path.getsize(file))
 print('Sum space: {}'.format(sum_bytes))
+
+# 1. include summing space occupied for folders with their content
+# Hint: use recersion
