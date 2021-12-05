08176df3072eff51f97ffe30909b5d3170414e0c
schus <ms113429@student.sgh.waw.pl>
2021-12-05 17:25:36 +0100

    Homework_17.12.2021

--- Homework_17.12.2021.py
+++ Homework_17.12.2021.py
@@ -1,11 +1,12 @@
 import os
+import csv
 from urllib.request import urlopen
 
-url = 'https://query1.finance.yahoo.com/v7/finance/download/MSFT?period1=1607083050&period2=1638619050&interval=1d&events=history&includeAdjustedClose=true'
+#url = 'https://query1.finance.yahoo.com/v7/finance/download/MSFT?period1=1607083050&period2=1638619050&interval=1d&events=history&includeAdjustedClose=true'
 
-local_path = os.path.join('data', 'MSFT.csv')
-with urlopen(url) as file, open(local_path, 'wb') as f:
-    f.write(file.read())
+#local_path = os.path.join('data', 'MSFT.csv')
+#with urlopen(url) as file, open(local_path, 'wb') as f:
+    #f.write(file.read())
 
 #repeat this for every file
 
