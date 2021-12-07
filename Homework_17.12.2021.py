ab267deea207740d6cf9eb54c615785aa6050d0a
schus <ms113429@student.sgh.waw.pl>
2021-12-07 17:10:15 +0100

    Homework_17.12.2021, another try

--- Homework_17.12.2021.py
+++ Homework_17.12.2021.py
@@ -2,24 +2,42 @@ import os
 import csv
 from urllib.request import urlopen
 
-# url = 'https://query1.finance.yahoo.com/v7/finance/download/MSFT?period1=1607083050&period2=1638619050&interval=1d&events=history&includeAdjustedClose=true'
+#url = 'https://query1.finance.yahoo.com/v7/finance/download/IBM?period1=1607354575&period2=1638890575&interval=1d&events=history&includeAdjustedClose=true'
 
-# local_path = os.path.join('data', 'MSFT.csv')
-# with urlopen(url) as file, open(local_path, 'wb') as f:
-# f.write(file.read())
+#local_path = os.path.join('data', 'GOOG.csv')
+#with urlopen(url) as file, open(local_path, 'wb') as f:
+ #   f.write(file.read())
 
 # repeat this for every file to download the CSV files. As I did it before I put the commands as comments to not repeat it every time I run the program.
 
-if __name__ == '__main__':
+def get_stocks():
     GOOG = 'data/GOOG.csv'
     IBM = 'data/IBM.csv'
     MSFT = 'data/MSFT.csv'
-    with open(GOOG, newline='') as old_GOOG:
-        spamwriter = csv.writer(old_GOOG, delimitor = ' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        for i, row in enumerate(GOOG):
+    GOOG = []
+    IBM = []
+    MSFT = []
+    with open('GOOG') as old_GOOG:
+        for rowsG in old_GOOG:
+            rowsG.append(rowsG.strip())
+    return rowsG
+    with open('IBM') as old_IBM:
+        for rowsI in old_IBM:
+            rowsI.append(rowsI.strip())
+    return rowsI
+    with open('MSFT') as old_MSFT:
+        for rowsM in old_MSFT:
+            rowsM.append(rowsM.strip())
+    return rowsM
+    with open('GOOG', newline='') as old_GOOG:
+        writer = csv.writer(old_GOOG, quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow((['Close']-['Open'])/['Open'])
+        for i, row in enumerate('GOOG'):
             if i != 0:
-                row.append(float[10] / float(row[37]))
-                csv_writer.writerow(row)
+                row.append('changes in a day')
+                writer.writerow(row)
+        csv.saveas(new_GOOG)
+        close(old_GOOG and new_GOOG)
 #https://docs.python.org/3/library/csv.html
 
     #directory = os.path.join('c:\\', 'C:/Users/schus/PycharmProjects/pythonProject1/data')
