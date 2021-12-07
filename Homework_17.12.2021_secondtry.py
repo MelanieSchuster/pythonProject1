ab75e93a1b6670ba1aeda6184ca151e82964676f
schus <ms113429@student.sgh.waw.pl>
2021-12-07 18:41:44 +0100

    Homework_17.12.2021, another try

--- Homework_17.12.2021_secondtry.py
+++ Homework_17.12.2021_secondtry.py
@@ -10,24 +10,21 @@ from urllib.request import urlopen
 # repeat this for every file to download the CSV files. As I did it before I put the commands as comments to not repeat it every time I run the program.
 
 from csv import reader as csv_reader
+import json
 
 def csv_to_json(csv_path: str, headers: bool) -> list:
-    csv_path = 'C:/Users/schus/PycharmProjects/pythonProject1/data/GOOG.csv'
     json_data = []
-
-    try:
-        with open(csv_path, 'r') as file:
-            reader = csv_reader(file)
-            if headers:
-                columns = next(reader)
-            for row in reader:
-                row_data = {}
-                for i in range(len(row)):
-                    if headers:
-                        row_key = columns[i].lower()
-                    else:
-                        row_key = i
-                row_data[row_key] = row[i]
-            json_data.append(row_data)
-        print(csv_to_sjon)
+    with open('C:/Users/schus/PycharmProjects/pythonProject1/data/GOOG.csv', 'r') as file:
+        reader = csv_reader(file)
+        if headers:
+            columns = next(reader)
+        for row in reader:
+            row_data = {}
+            for i in range(len(row)):
+                if headers:
+                    row_key = columns[i].lower()
+                else:
+                    row_key = i
+            row_data[row_key] = row[i]
+        json_data.append(row_data)
     return json_data
